ec4d80c41afebebcdbe13eb80b0da0822d3db389
ManzDev <JoseRomanHernandez@gmail.com>
2021-03-28 21:46:27 +0100

    Twitch (Live)

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const audio = new Audio("/gravity.mp3");
+const offPieces = [
+  { name: ".hat-container", delay: 1000 },
+  { name: ".eye", delay: 2000 },
+  { name: ".eyelashes-bottom", delay: 2300 },
+  { name: ".eyelashes-top", delay: 2200 },
+  { name: ".left.arm", delay: 4000 },
+  { name: ".right.arm", delay: 4000 },
+  { name: ".left.leg", delay: 6000 },
+  { name: ".right.leg", delay: 6000 },
+  { name: ".tie", delay: 8000 }
+];
+const container = document.querySelector(".container");
+const triangle = document.querySelector("body");
+audio.volume = 0.5;
+
+triangle.addEventListener("click", () => {
+  container.classList.add("off");
+  audio.play();
+  offPieces.forEach(piece => {
+    const pieceElement = document.querySelector(piece.name);
+    setTimeout(() => pieceElement.classList.add("on"), piece.delay);
+  });
+  setTimeout(() => container.classList.add("dark"), 8000);
+});
